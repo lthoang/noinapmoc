e8b1ebfd2223b17f78a241f7b252e9859905eaec
Hoang Le <trhoanglee@gmail.com>
2023-09-16 21:48:46 +0800

    Add explanation data

--- docs/main.js
+++ docs/main.js
@@ -1,28 +1,95 @@
+const regexp = /\[[\w\/ -]+\]/g;
 var explanationIndices = [];
 var totalCount = 0;
 var currentExplanationIndex = 0;
-var explanationTemplate = '<div class="row"><div class="col-5"><figure style="text-align: center"><h5>Recommendation product: <a href="https://www.amazon.com/dp/${recommended}">${recommended}</a></h5><figcaption>${recommendedTitle}</figcaption><a href="https://www.amazon.com/dp/${recommended}" target="_blank" tooltip="Click to view product page"><img src="${recommendedImg}"style="margin-bottom: 1rem; max-width: 50%;" alt=""></a></figure></div><div class="col-2"></div><div class="col-5"><figure style="text-align: center"><h5>Reference Item: <a href="https://www.amazon.com/dp/${reference}">${reference}</a></h5><figcaption>${referenceTitle}</figcaption><a href="https://www.amazon.com/dp/${reference}" target="_blank" tooltip="Click to view product page"><img src="${referenceImg}"style="margin-bottom: 1rem; max-width: 50%;" alt=""></a></figure></div></div><div class="row"><div class="col" style="text-align: center"><h4>Explanation</h4><p>${explanationText}</p></div></div>';
+var explanationTemplate = '';
+var queryParams = new URLSearchParams(window.location.search);
+var explanationPath = queryParams.get("path") ? queryParams.get("path") : './data/cellphone/u-explanation.json';
+var explanationDir = queryParams.get("dir") ? queryParams.get("dir") : './data/cellphone/u-explanations';
+var itemDir = queryParams.get("itemDir") ? queryParams.get("itemDir") : './data/cellphone/items';
+var explanationIdx = document.getElementById('explanation-id');
+
+var selectData = (name, dataPath, dataDir, newItemDir) => {
+    explanationPath = dataPath;
+    explanationDir = dataDir;
+    itemDir = newItemDir;
+    document.getElementById('data-selection').innerText = name;
+    renderExplanation();
+}
+
+var updateFormValue = () => {
+    explanationIdx.value = window.location.href;
+}
+
+var placeholder = () => {
+    var recommended = {
+        title: "recommended item",
+        imUrl: "./data/placeholder-image.png",
+    };
+    var reference = {
+        title: "reference item",
+        imUrl: "./data/placeholder-image.png",
+    };
+    var aspect = "";
+    var explanation = {
+        "explanation": "Loading...",
+    };
+    document.getElementById("explanation").innerHTML = eval('`' + explanationTemplate + '`');
+}
+
+
+var postCorrection = (explanationText) => {
+    const matches = [...explanationText.matchAll(regexp)];
+    if (matches[2][0] === matches[4][0]) {
+        explanationText = explanationText.replace(/is (better|worse) than/, "is similar to");
+    }
+    return explanationText;
+}
 
 var displayExplanation = (idx) => {
-    fetch(`./data/explanations/${explanationIndices[idx]}`).then((response) => response.json()).then((json) => {
-        var explanation = json;
-        var userItemAspect = explanationIndices[idx].split(",");
-        var user = userItemAspect[0];
-        var recommended = userItemAspect[1];
-        var aspect = userItemAspect[2];
-        
-        var explanationText = explanation["explanation"];
-        var reference = explanation["jid"];
-        fetch(`./data/items/${recommended}`).then((response) => response.json()).then((item) => {
-            var recommendedTitle = item["title"];
-            var recommendedImg = item["imUrl"];
-            fetch(`./data/items/${reference}`).then((response) => response.json()).then((item) => {
-                var referenceTitle = item["title"];
-                var referenceImg = item["imUrl"];
-                document.getElementById("explanation").innerHTML = eval('`' + explanationTemplate + '`');
-            });
+    placeholder();
+    fetch(`${explanationDir}/${explanationIndices[idx]}`)
+        .then((response) => response.json())
+        .then((explanation) => {
+            var skipAspects = localStorage.getItem('skipAspects');
+            var recommended = {};
+            var reference = {};
+            for (let index = 0; index < explanation['explanation'].length; index++) {
+                explanation['explanation'][index] = postCorrection(explanation['explanation'][index]);
+                const matches = [...explanation['explanation'][index].matchAll(regexp)];
+                if (skipAspects && skipAspects.length > 0 && skipAspects.includes(matches[1][0].replace("[", "").replace("]", ""))) {
+                    nextExplanation();
+                    return;
+                }
+            }
+            fetch(`${itemDir}/${explanation['iid']}`)
+                .then((response) => response.json())
+                .then((item) => {
+                    recommended = item;
+                    fetch(`${itemDir}/${explanation['jid']}`)
+                        .then((response) => response.json())
+                        .then((item) => {
+                            reference = item;
+                            document.getElementById("explanation").innerHTML = eval('`' + explanationTemplate + '`');
+                            queryParams.set('explanation', explanationIndices[idx]);
+                            history.replaceState(null, null, "?" + queryParams.toString());
+                            updateFormValue();
+                        }).catch((error) => {
+                            document.getElementById("explanation").innerHTML = eval('`' + explanationTemplate + '`');
+                            queryParams.set('explanation', explanationIndices[idx]);
+                            history.replaceState(null, null, "?" + queryParams.toString());
+                            updateFormValue();
+                        });
+                })
+                .catch((error) => {
+                    document.getElementById("explanation").innerHTML = eval('`' + explanationTemplate + '`');
+                    queryParams.set('explanation', explanationIndices[idx]);
+                    history.replaceState(null, null, "?" + queryParams.toString());
+                    updateFormValue();
+                });
+        }).catch((error) => {
+            console.log(error);
         });
-    });
 }
 
 var prevExplanation = () => {
@@ -39,8 +106,61 @@ var nextExplanation = () => {
     }
 }
 
-fetch('./data/explanation.json').then((response) => response.json()).then((json) => {
-    explanationIndices = json;
-    totalCount = explanationIndices.length;
-    displayExplanation(currentExplanationIndex);
+var renderExplanation = () => {
+    queryParams.set('dir', explanationDir);
+    queryParams.set('path', explanationPath);
+    queryParams.set('itemDir', itemDir);
+    history.replaceState(null, null, "?" + queryParams.toString());
+    fetch(explanationPath)
+        .then((response) => response.json())
+        .then((json) => {
+            explanationIndices = json;
+            totalCount = explanationIndices.length;
+            var explanationQuery = queryParams.get('explanation');
+            if (explanationQuery) {
+                var explanationIndex = explanationIndices.indexOf(explanationQuery);
+                if (explanationIndex > -1) {
+                    currentExplanationIndex = explanationIndex
+                    updateFormValue();
+                }
+            }
+            displayExplanation(currentExplanationIndex);
+        });
+
+}
+
+fetch(`./data/template.html`)
+    .then((response) => response.text())
+    .then((template) => {
+        explanationTemplate = template;
+        placeholder();
+        renderExplanation();
+    });
+
+
+var toastNotifier = document.getElementById('liveToast');
+var toast = new bootstrap.Toast(toastNotifier);
+
+
+window.addEventListener("load", function () {
+    const form = document.getElementById('record-explanation-form');
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const data = new FormData(form);
+        const action = e.target.action;
+        fetch(action, {
+            method: 'POST',
+            body: data,
+        }).then(() => {
+            const savedValue = Object.fromEntries(data)['explanation'];
+            document.getElementById('liveToastText').innerText = decodeURIComponent(savedValue.split('=').at(-1)) + ' is saved!';
+            toast.show();
+        });
+    });
 });
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
